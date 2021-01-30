9bc8c3e04010001290ed61429f6a8a60a80791f7
shailesh shinde <shai1esh.shinde@yahoo.com>
2021-01-30 12:28:42 +1100

    initial commit

---
+++ src/main/java/com/svs/dockermeans/DockermeansApplication.java
@@ -0,0 +1,13 @@
+package com.svs.dockermeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockermeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockermeansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svs/dockermeans/DockermeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svs.dockermeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockermeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
